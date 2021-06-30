48fc889191e1268bead31780dc1e233ef67f196a
Adam Presley <adam@adampresley.com>
2021-06-30 18:54:25 -0500

    Fixed invalid reference to model in hook code

--- cmd/fireplace-hook/fireplace-hook.go
+++ cmd/fireplace-hook/fireplace-hook.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/app-nerds/fireplace/pkg/logentry"
+	"github.com/app-nerds/fireplace/api/logentry/logentry_models"
 	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 )
@@ -32,22 +32,22 @@ func NewFireplaceHook(config *FireplaceHookConfig) *FireplaceHook {
 func (h *FireplaceHook) Fire(entry *logrus.Entry) error {
 	var err error
 
-	data := &logentry.CreateLogEntryRequest{
+	data := &logentry_models.CreateLogEntryRequest{
 		Application: h.config.Application,
 		Level:       entry.Level.String(),
 		Time:        entry.Time.UTC().Format(time.RFC3339),
 		Message:     entry.Message,
-		Details:     make(logentry.LogEntryDetailItemCollection, 0, 10),
+		Details:     make(logentry_models.LogEntryDetailItemCollection, 0, 10),
 	}
 
 	for key, value := range entry.Data {
 		if errorData, isError := value.(error); logrus.ErrorKey == key && value != nil && isError {
-			data.Details = append(data.Details, &logentry.LogEntryDetailItem{
+			data.Details = append(data.Details, &logentry_models.LogEntryDetailItem{
 				Key:   key,
 				Value: errorData.Error(),
 			})
 		} else {
-			data.Details = append(data.Details, &logentry.LogEntryDetailItem{
+			data.Details = append(data.Details, &logentry_models.LogEntryDetailItem{
 				Key:   key,
 				Value: fmt.Sprintf("%v", value),
 			})
@@ -73,7 +73,7 @@ func (h *FireplaceHook) Levels() []logrus.Level {
 	}
 }
 
-func (h *FireplaceHook) send(entry *logentry.CreateLogEntryRequest) error {
+func (h *FireplaceHook) send(entry *logentry_models.CreateLogEntryRequest) error {
 	var err error
 	var entryJSON []byte
 	var response *http.Response
