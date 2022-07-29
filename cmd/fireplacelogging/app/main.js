be58a69132d52278c72fa5c030b07d935dfd753b
Adam Presley <adam@adampresley.com>
2022-07-28 21:43:03 -0500

    Servers are displaying

--- cmd/fireplacelogging/app/main.js
+++ cmd/fireplacelogging/app/main.js
@@ -2,7 +2,8 @@ import Home from "./static/views/Home.js";
 import ViewLogs from "./static/views/ViewLogs.js";
 import ManageServers from "./static/views/ManageServers.js";
 
-import { application } from "./static/js/libraries/nerdwebjs/nerdwebjs.min.js";
+import { application, GraphQL, fetch } from "./static/js/libraries/nerdwebjs/nerdwebjs.min.js";
+import nerdadmin from "./static/js/libraries/nerdadmin/nerdadmin.min.js";
 
 const routes = [
   { path: "/", view: Home },
@@ -11,4 +12,14 @@ const routes = [
 ];
 
 const app = application("#app", routes);
+
+app.injectParams(() => {
+  return {
+    graphql: new GraphQL("http://localhost:8080/query", fetch),
+    nerdalert: nerdadmin.nerdalert(),
+    nerdconfirm: nerdadmin.nerdconfirm(),
+    nerdshim: nerdadmin.nerdshim(),
+  };
+});
+
 app.go();
--- cmd/fireplacelogging/app/static/views/ManageServers.js
+++ cmd/fireplacelogging/app/static/views/ManageServers.js
@@ -19,7 +19,74 @@ export default class ManageServers extends BaseView {
   }
 
   async afterRender() {
+    await this.#loadAndRender();
     feather.replace();
+  }
+
+  async #getServers() {
+    let query = `getServers() {
+      id
+      description
+      serverName
+      url
+    }`;
+
+    try {
+      const response = await this.params.graphql.query(query);
+      return response.data.getServers;
+    } catch (e) {
+      console.log(e);
+      this.params.nerdalert.error(e.message);
+    }
+  }
+
+  async #loadAndRender() {
+    const servers = await this.#getServers();
+    this.#renderServerCards(servers);
+  }
+
+  #renderServerCards(servers) {
+    let container = document.getElementById("servers");
+
+    servers.forEach(s => {
+      const card = this.#renderCard(s);
+      container.insertAdjacentElement("beforeend", card);
+    });
+  }
+
+  #renderCard(server) {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+
+    const title = document.createElement("h3");
+    title.innerText = server.serverName;
+
+    const content = document.createElement("div");
+    content.classList.add("content");
+
+    const link = document.createElement("p");
+    link.innerHTML = `<a href="${server.url}" target="_blank">${server.url}</a>`;
+
+    const description = document.createElement("p");
+    description.innerHTML = server.description;
+
+    const editButton = document.createElement("button");
+    editButton.classList.add("action-button");
+    editButton.innerHTML = `<i data-feather="edit-2"></i> Edit`;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-button");
+    deleteButton.innerHTML = `<i data-feather="trash-2"></i>`;
+
+    content.insertAdjacentElement("beforeend", link);
+    content.insertAdjacentElement("beforeend", description);
+    content.insertAdjacentElement("beforeend", editButton);
+    content.insertAdjacentElement("beforeend", deleteButton);
+
+    cardDiv.insertAdjacentElement("beforeend", title);
+    cardDiv.insertAdjacentElement("beforeend", content);
+
+    return cardDiv;
   }
 }
 
